d09fef0c7b8449f815bbae5c2fa6b645523b709c
Sofia Mentado <sofyment99@gmail.com>
2023-06-26 11:53:43 -0700

    Question3 | ky8

--- js/script.js
+++ js/script.js
@@ -14,3 +14,16 @@ const well = (x) => {
     ? "Publish!"
     : "I smell a series!";
 };
+// Question3 | ky8
+/*Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string): 
+
+Example: (Input1, Input2 -->Output)
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"*/
+
+function sumStr(a, b) {
+  return String(Number(a) + Number(b));
+}
