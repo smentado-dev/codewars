5c2f7486d713a67db0aeb307b48fbce10a319a64
Sofia Mentado <sofyment99@gmail.com>
2023-06-27 17:15:20 -0700

    Question4 | ky8

--- js/script.js
+++ js/script.js
@@ -27,3 +27,16 @@ Example: (Input1, Input2 -->Output)
 function sumStr(a, b) {
   return String(Number(a) + Number(b));
 }
+// Question4 | ky8
+/*Unfinished Loop - Bug Fixing #1
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!*/
+
+function createArray(number) {
+  var newArray = [];
+
+  for (var counter = 1; counter <= number; counter++) {
+    newArray.push(counter);
+  }
+
+  return newArray;
+}
