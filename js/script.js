1004f1e000d1d88e868a04ff15bda007a110589b
Sofia Mentado <sofyment99@gmail.com>
2023-07-03 17:49:39 -0700

    Question10 | ky8

--- js/script.js
+++ js/script.js
@@ -145,3 +145,11 @@ function findMultiples(int, limit) {
 
   return result;
 }
+// Question10 | ky8 | Grasshopper - Debug sayHello
+
+/*The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock*/
+const sayHello = (name) => `Hello, ${name}`;
