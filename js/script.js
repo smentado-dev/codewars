a8c52387ea153d86ed2480520773da0a7dc13eb8
Sofia Mentado <sofyment99@gmail.com>
2023-07-17 22:46:22 -0500

    Question23 | ky8 | Is the string uppercase?

--- js/script.js
+++ js/script.js
@@ -329,3 +329,17 @@ var championsLeagueGoals = 10;
 var copaDelReyGoals = 5;
 
 var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+// Question23 | ky8 | Is the string uppercase?
+
+/*Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True*/
+String.prototype.isUpperCase = function () {
+  return this == this.toUpperCase();
+};
