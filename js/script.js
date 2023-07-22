d21162f1aef796b54754ca17c7d933671e452985
Sofia Mentado <sofyment99@gmail.com>
2023-07-22 15:56:32 -0500

    Question28 | ky8 | The Feast of Many Beasts

--- js/script.js
+++ js/script.js
@@ -438,3 +438,17 @@ cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
 function enough(cap, on, wait) {
   return Math.max(wait + on - cap, 0);
 }
+// Question28 | ky8 | The Feast of Many Beasts
+
+/*
+All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+*/
+function feast(beast, dish) {
+  return (
+    beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+  );
+}
