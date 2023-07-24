b4d86aca127acd2a3bad24bac7505cea1ae51368
Sofia Mentado <sofyment99@gmail.com>
2023-07-24 11:53:32 -0500

    Question30 | ky8 | Twice as old

--- js/script.js
+++ js/script.js
@@ -464,3 +464,15 @@ Assume the test input string is always a valid string. For example, the test inp
 function getAge(inputString) {
   return parseInt(inputString);
 }
+// Question30 | ky8 | Twice as old
+
+/*
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+*/
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - 2 * sonYearsOld);
+}
