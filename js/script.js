6eaf159e88c7eecc7322408793fbd9e37e9efae4
Sofia Mentado <sofyment99@gmail.com>
2023-07-29 19:03:58 -0500

    Question35 | ky8 | Grasshopper - Messi goals function

--- js/script.js
+++ js/script.js
@@ -591,3 +591,23 @@ const doubleChar = (str) =>
     .split("")
     .map((c) => c + c)
     .join("");
+    // Question35 | ky8 | Grasshopper - Messi goals function
+/*
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+*/
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+    
