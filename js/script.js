747116ee1ae06c211fb1af4e9131f04efab7aa89
Sofia Mentado <sofyment99@gmail.com>
2023-07-31 10:36:35 -0500

    Question37 | ky8 | Count the Monkeys!

--- js/script.js
+++ js/script.js
@@ -622,5 +622,23 @@ Return the total figure the individual will receive as a string prefixed with "�
 function bonusTime(salary, bonus) {
   return bonus ? `£${10 * salary}` : `£${salary}`;
 }
+// Question37 | ky8 | Count the Monkeys!
+/*
+You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+For example(Input --> Output):
+
+10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ 1 --> [1]
+*/
+function monkeyCount(n) {
+  var monkeys = [];
+  for (var i = 1; i < n + 1; i++) {
+    monkeys.push(i);
+  }
+  return monkeys;
+}
 
     
