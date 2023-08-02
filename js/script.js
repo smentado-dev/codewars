5605fc877587146c773145c42d8a7e2c4a5f451e
Sofia Mentado <sofyment99@gmail.com>
2023-08-02 17:22:54 -0500

    Question39 | ky8 |Question40 | ky8 |

--- js/script.js
+++ js/script.js
@@ -654,5 +654,48 @@ false    | true     => false
 false    | false    => false
 */
 const setAlarm = (employed, vacation) => employed && !vacation;
+// Question39 | ky8 | Thinkful - Logic Drills: Traffic light
 
+/*
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow.
+*/
+
+//function passes in string (current state of light) -> 'string', no num, no caps
+//return -> string (displays next light)
+//
+function nextLight(currentLight) {
+  //currentLight -> change to nextLight
+  if (currentLight === "red") {
+    return "green";
+  } else if (currentLight === "green") {
+    return "yellow";
+  } else {
+    return "red";
+  }
+}
+nextLight("red");
+//test cases
+console.log(nextLight("red"), "green");
+console.log(nextLight("green"), "yellow");
+console.log(nextLight("yellow"), "red");
+
+// function updateLight(current) {
+// return current === 'yellow' ? 'red' : current === 'green' ? 'yellow' : 'green';
+// }
+// Question40 | ky8 | Third Angle of a Triangle
+
+/*
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+*/
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
     
