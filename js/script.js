d652ce711e419bb8fac38f3ab3d35cb66f3208b3
Sofia Mentado <sofyment99@gmail.com>
2023-08-09 13:48:24 -0500

    Question45 | ky8 | Area or Perimeter

--- js/script.js
+++ js/script.js
@@ -803,3 +803,17 @@ function getGrade(s1, s2, s3) {
   let s = (s1 + s2 + s3) / 3;
   return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F";
 }
+/*
+Question45 | ky8 | Area or Perimeter
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+Example(Input1, Input2 --> Output):
+
+6, 10 --> 32
+3, 3 --> 9
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+*/
+const areaOrPerimeter = function (l, w) {
+  return l == w ? l * w : 2 * (l + w);
+};
