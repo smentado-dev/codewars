d3f154a113ab00bbeef90fe89d7b96c4a0bfc5e1
Sofia Mentado <sofyment99@gmail.com>
2023-08-10 09:57:03 -0500

    Question46 | ky8 | Sum Mixed Array

--- js/script.js
+++ js/script.js
@@ -817,3 +817,11 @@ Note: for the purposes of this kata you will assume that it is a square if its l
 const areaOrPerimeter = function (l, w) {
   return l == w ? l * w : 2 * (l + w);
 };
+/*
+Question46 | ky8 | Sum Mixed Array
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+Return your answer as a number.
+*/
+function sumMix(x) {
+  return x.map((a) => +a).reduce((a, b) => a + b);
+}
