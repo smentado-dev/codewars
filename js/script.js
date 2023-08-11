3068545cb5f8e9c983b3b9e1cd9737a2c72b761f
Sofia Mentado <sofyment99@gmail.com>
2023-08-11 16:16:06 -0500

    Question47 | ky8 | Array plus array

--- js/script.js
+++ js/script.js
@@ -825,3 +825,11 @@ Return your answer as a number.
 function sumMix(x) {
   return x.map((a) => +a).reduce((a, b) => a + b);
 }
+/*
+Question47 | ky8 | Array plus array
+I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+P.S. Each array includes only integer numbers. Output is a number too.
+*/
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
